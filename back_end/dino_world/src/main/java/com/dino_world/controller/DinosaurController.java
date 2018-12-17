a9f0200051249807c6af140092f858b2f23cc561
AndrewCarracher <andrew.carracher@hotmail.co.uk>
2018-12-17 12:04:36 +0000

    routes finished (I hope)

--- back_end/dino_world/src/main/java/com/dino_world/controller/DinosaurController.java
+++ back_end/dino_world/src/main/java/com/dino_world/controller/DinosaurController.java
@@ -4,6 +4,7 @@ import com.dino_world.models.Dinosaur;
 import com.dino_world.repositories.dinosaurs.DinosaurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,15 @@ public class DinosaurController {
     @GetMapping(value = "/")
     public List<Dinosaur> allDinosaurs(){
         return dinosaurRepository.allDinosaurs();
+    }
+
+    @GetMapping(value = "/{name}")
+    public List<Dinosaur> getDinosaurByName(@PathVariable String name){
+        return dinosaurRepository.getDinosaurByName(name);
+    }
+
+    @GetMapping(value = "/feed/{name}")
+    public void feedDinosaur(@PathVariable String name){
+        dinosaurRepository.feedDinosaur(name);
     }
 }
--- back_end/dino_world/src/main/java/com/dino_world/controller/PaddockController.java
+++ back_end/dino_world/src/main/java/com/dino_world/controller/PaddockController.java
@@ -1,9 +1,11 @@
 package com.dino_world.controller;
 
+import com.dino_world.models.Dinosaur;
 import com.dino_world.models.Paddock;
 import com.dino_world.repositories.paddocks.PaddockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +22,24 @@ public class PaddockController {
     public List<Paddock> allPadocks(){
         return paddockRepository.allPaddocks();
     }
+
+    @GetMapping(value = "check_space/{name}")
+    public int freeSpace(@PathVariable String name){
+        return paddockRepository.freeSpace(name);
+    }
+
+    @GetMapping(value = "check_compatability/{dino_name}/{paddock_name}")
+    public boolean checkCompatibility(@PathVariable String dinosaurName, String paddockName){
+        return paddockRepository.checkCompatibility(dinosaurName, paddockName);
+    }
+
+   @GetMapping(value = "add_dino/{dino_name/{paddock_name}")
+   public void addDinosaur(@PathVariable String dinosaurName, String paddockName){
+        paddockRepository.addDinosaur(dinosaurName, paddockName);
+   }
+
+   @GetMapping(value = "remove_dino/{dino_name}/{dino_type}/{paddock_name}")
+   public Dinosaur removeDinosaur(@PathVariable String name, String type, String paddockName){
+        return paddockRepository.removeDinosaur(name, type, paddockName);
+   }
 }
